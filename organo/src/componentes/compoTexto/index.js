f456b8e3333eb21ea7c4f77578f7735654ca63aa
Simao-dev <pedrosimaocontato@gmail.com>
2025-08-28 21:36:27 -0300

    aulas 6 e 7

--- organo/src/componentes/compoTexto/index.js
+++ organo/src/componentes/compoTexto/index.js
@@ -1,11 +1,13 @@
 import './campoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
 
     return(
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite o seu nome'/>
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 
